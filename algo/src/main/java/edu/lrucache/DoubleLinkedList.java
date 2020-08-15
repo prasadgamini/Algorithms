dbaf449c1d0923f07d54bd10a9e5b83418c8c6b9
Prasad Gamini <Prasad.Gamini@aa.com>
2020-08-14 19:12:50 -0500

    Adding DLL for LRU cache implementation

---
+++ algo/src/main/java/edu/lrucache/DoubleLinkedList.java
@@ -0,0 +1,43 @@
+package edu.lrucache;
+
+public class DoubleLinkedList {
+    private Node head, tail;
+
+    public void addNode(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (head == null && tail == null) {
+            head = tail = null;
+        } else {
+            node.next = head;
+            head.previous = node;
+            head = node;
+        }
+    }
+
+    public void removeNode(Node node) {
+        if (head == node && tail == node) {
+            head = tail = null;
+        } else if (head == node) {
+            head = head.next;
+            head.previous = null;
+        } else if (tail == node) {
+            tail = tail.previous;
+            tail.next = null;
+        } else {
+            node.previous.next = node.next;
+            node.next.previous = node.previous;
+        }
+        node.previous = node.next = null;
+    }
+
+    public void removeTailNode() {
+        this.removeNode(this.tail);
+    }
+
+    public void moveNodeToHead(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+}
---
+++ algo/src/main/java/edu/lrucache/Node.java
@@ -0,0 +1,8 @@
+package edu.lrucache;
+
+public class Node {
+    Node previous;
+    int key;
+    int value;
+    Node next;
+}
