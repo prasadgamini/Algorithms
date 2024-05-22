bce8c734f7048a3c3c1f58381d742136a67f1f75
Prasad Gamini <>
2024-05-22 13:13:44 -0500

    Added another test for GoodNodeCounter

--- algo/src/test/java/edu/trees/GoodNodeCounterTest.java
+++ algo/src/test/java/edu/trees/GoodNodeCounterTest.java
@@ -6,9 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GoodNodeCounterTest {
     @Test
-    void testGoodNodeCounter() {
-        TreeNode root = createTree();
+    void testGoodNodeCounterOne() {
+        // Construct the tree [3, 1, 4, 3, null, 1, 5]
+        /*
+         * ........3
+         * ......./ \
+         * ......1   4
+         * ...../   / \
+         * ....3   1   5
+         */
+        TreeNode root = new TreeNode(3,
+                new TreeNode(1,
+                        new TreeNode(3, null, null),
+                        null
+                ),
+                new TreeNode(4,
+                        new TreeNode(1, null, null),
+                        new TreeNode(5, null, null)
+                )
+        );
         assertEquals(4, GoodNodeCounter.goodNodes(root));
+    }
+
+    @Test
+    void testGoodNodeCounterTwo() {
+        /*
+                    3
+                   /
+                  3
+                 / \
+                4   2
+         */
+        TreeNode root = new TreeNode(3,
+                new TreeNode(3,
+                        new TreeNode(4, null, null),
+                        new TreeNode(2, null, null)
+                ),
+                null
+        );
+        assertEquals(3, GoodNodeCounter.goodNodes(root));
     }
 
 
